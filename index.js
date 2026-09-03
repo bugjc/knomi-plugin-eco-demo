459c7c80c0d1369517b1725913323b3cbba4a657
Aoki <qing.muyi@foxmail.com>
2026-09-04 02:39:28 +0800

    v0.2.0

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = {
   id: 'eco-demo',
   name: 'Eco Demo',
-  version: '0.1.5',
+  version: '0.2.0',
   async activate(ctx) { ctx.registerAgentTool(
       { name: 'eco_tool', description: 'Eco demo verification tool', parameters: { type: 'object', properties: {}, required: [] } },
       async () => 'ok'
     ) },
   async deactivate() {},
-  ping() { return 'pong 0.1.5' },
+  ping() { return 'pong 0.2.0' },
 }
